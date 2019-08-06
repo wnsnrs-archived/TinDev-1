95e445a0cb829eaca2b4b89bcf50bf491523dd26
Will Douglas <willdowglas@gmail.com>
2019-08-06 10:38:44 -0300

    adding controller Dev

---
+++ src/controllers/DevController.js
@@ -0,0 +1,27 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+
+module.exports = {
+  async store(request, response){
+    const { username } = request.body
+
+    const userExists = await Dev.findOne({ user: username })
+
+    if (userExists) {
+      return response.json(userExists)
+    }
+
+    const githubResponse = await axios.get(`https://api.github.com/users/${username}`)
+
+    const { name, bio, avatar_url: avatar } = githubResponse.data
+
+    const dev = await Dev.create({
+      name,
+      user: username,
+      bio,
+      avatar
+    })
+
+    return response.json(dev)
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 const express = require('express')
+const DevController = require('./controllers/DevController')
 
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-  return response.json({
-    "message": "test"
-  })
-})
-
-routes.post('/devs', (request, response) => {
-  return response.json(request.body)
-})
+routes.post('/devs', DevController.store)
 
 module.exports = routes
