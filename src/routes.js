ab02640f8fe9a35f861f7b5393b49d34b0346c49
Will Douglas <willdowglas@gmail.com>
2019-08-06 12:48:08 -0300

    updating routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const DevController = require('./controllers/DevController')
 const LikeController = require('./controllers/LikeController')
+const DislikeController = require('./controllers/DislikeController')
+const ResetController = require('./controllers/ResetController')
 
 const routes = express.Router()
 
 routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
+routes.put('/devs', ResetController.update)
 
 routes.post('/devs/:devId/like', LikeController.store)
-routes.post('/devs/:devId/disklike', DislikeController.store)
+routes.post('/devs/:devId/dislike', DislikeController.store)
 
 module.exports = routes
