228a71f92e7284cbdc48c1dd84081274b0c50ad7
Will Douglas <willdowglas@gmail.com>
2019-08-06 12:48:15 -0300

    adding cors to server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const routes = require('./routes')
 
 const server = express()
@@ -8,6 +9,7 @@ mongoose.connect('mongodb+srv://willdowglas:MTeEOAUJuK02AM5T@cluster0-ljmte.mong
   useNewUrlParser: true
 })
 
+server.use(cors())
 server.use(express.json())
 server.use(routes)
 
