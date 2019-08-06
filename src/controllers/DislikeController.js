494575c78c9358289bd4c8681a974fad0a1d9c42
Will Douglas <willdowglas@gmail.com>
2019-08-06 12:50:57 -0300

    removing console.loggers

--- src/controllers/DislikeController.js
+++ src/controllers/DislikeController.js
@@ -4,12 +4,10 @@ module.exports = {
   async store(request, response) {
     const { user_id } = request.headers
     const { devId } = request.params
-    console.log(user_id)
 
     const loggedDev = await Dev.findById(user_id)
-    console.log(loggedDev)
     const targetDev = await Dev.findById(devId)
-    console.log(targetDev)
+
     if (!targetDev) {
       return response.status(400).json({ error: 'Dev not exists'})
     }
