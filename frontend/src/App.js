90e294469b9c4b7f6128d4c4fdc01f99a7607df2
Will Douglas <willdowglas@gmail.com>
2019-08-10 00:50:30 -0300

    adding component Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
+import Login from './pages/Login'
+
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <Login />
   );
 }
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Login.css'
+import logo from '../assets/logo.svg'
+
+export default function Login() {
+  return (
+    <img src={logo} alt='tindev' />
+  )
+}
