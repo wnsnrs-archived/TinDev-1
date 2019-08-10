4997cad0384458628e0fae0e158c99550c16e04a
Will Douglas <willdowglas@gmail.com>
2019-08-10 13:34:26 -0300

    adding navigation route when submit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import logo from '../assets/logo.svg'
 
-export default function Login() {
+export default function Login(props) {
+  const [username, setUsername] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    props.history.push('/main')
+  }
+
   return (
     <div className="login-container">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt='tindev' />
         <input
           placeholder="Digite seu user do Github"
+          value={username}
+          onChange={event => setUsername(event.target.value)}
         />
         <button type="submit">Enviar</button>
       </form>
