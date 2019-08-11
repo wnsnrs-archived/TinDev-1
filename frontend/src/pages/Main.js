45deb33d726c143edc027b6ec2f72e54348ca923
Will Douglas <willdowglas@gmail.com>
2019-08-11 18:11:13 -0300

    adding feature like/dislike/reset

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -9,23 +9,37 @@ import dislike from '../assets/dislike.svg'
 export default function Main(props) {
   const [users, setUsers] = useState([])
 
+  
   useEffect(() => {
     async function loadUsers() {
-      const response = await api.get('/devs', {
-        headers: {
-          user_id: props.match.params.id,
-        }
-      })
-
+      const response = await api.get('/devs', { headers: { user_id: props.match.params.id,} })
       setUsers(response.data)
     }
+      loadUsers()
+    }, [props.match.params.id])
 
-    loadUsers()
-  }, [props.match.params.id])
+  async function handleLike(id) {
+    await api.post(`/devs/${id}/like`, null, { headers: { user_id: props.match.params.id } })
+
+    setUsers(users.filter(user => user._id !== id))
+  }
+
+  async function handleDislike(id) {
+    await api.post(`/devs/${id}/dislike`, null, { headers: { user_id: props.match.params.id } })
+
+    setUsers(users.filter(user => user._id !== id))
+  }
+
+  async function handleReset(){
+    await api.put('/devs', null, { headers: { user_id: props.match.params.id } })
+    const response = await api.get('/devs', { headers: { user_id: props.match.params.id } })
+    setUsers(response.data)
+  }
 
   return (
     <div className="main-container">
       <img src={logo} alt="Tindev"/>
+      { users.length > 0 ? (
       <ul>
         {users.map(user => (
           <li key={user._id}>
@@ -35,16 +49,24 @@ export default function Main(props) {
             <p>{user.bio}</p>
           </footer>
           <div className="buttons">
-            <button type="button">
+            <button type="button" onClick={() => handleDislike(user._id)}>
               <img src={dislike} alt="dislike"/>
             </button>
-            <button type="button">
+            <button type="button" onClick={() => handleLike(user._id)}>
               <img src={like} alt="Like"/>
             </button>
           </div>
         </li>
         ))}
       </ul>
+        ) : (
+          <div>
+            <div className="empty">Acabou. :(</div>
+            <div className="buttons">
+              <button type="button" className="btn" onClick={() => {handleReset()}}>Reset</button>
+            </div>
+          </div>
+        ) }
     </div>
   )
 }
